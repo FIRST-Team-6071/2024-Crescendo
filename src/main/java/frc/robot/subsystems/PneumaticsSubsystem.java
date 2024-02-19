2853258e4a9571cb39fc53974b61ed6c8aa9d528
Pickles <108486434+Vlasic-Pickles@users.noreply.github.com>
2024-02-19 16:37:14 -0500

    Made it an actual sub system and hopefully didnt mess up code

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -1,36 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Constants;
 
-//two solenoids, SingleSolenoids.
-//on when extended. will propbably be set automatically to off*
-public class PneumaticsSubsystem {
-    //Solenoids initialization
-    private Solenoid leftSolenoid = new Solenoid(
-        Constants.PneumaticsSubsystem.leftSolenoidID, 
-        PneumaticsModuleType.REVPH, //Recycled REVPH type from old code idk what it is lol
-        Constants.PneumaticsSubsystem.SolenoidChannel
-    );
-    private Solenoid RightSolenoid = new Solenoid(
-        Constants.PneumaticsSubsystem.RightSolenoidID, 
-        PneumaticsModuleType.REVPH, 
-        Constants.PneumaticsSubsystem.SolenoidChannel
-    );
-    //Rev Pneumatic controller initialization
-    private PneumaticsControlModule PnController = new PneumaticsControlModule( 
-        Constants.PneumaticsSubsystem.PnControllerID);
+public class PneumaticsSubsystem extends SubsystemBase {
+  /** Creates a new PneumaticsSubsystem. */
+  public PneumaticsSubsystem() {}
 
-    //methods to open and close the claws
-    public static void Openclaws() {
-        leftSolenoid.set(true);
-        RightSolenoid.set(true);
-    }
+  //Solenoids initialization
+  private Solenoid leftSolenoid = new Solenoid(
+      Constants.PneumaticsSubsystem.leftSolenoidID, 
+      PneumaticsModuleType.REVPH, //Recycled REVPH type from old code idk what it is lol
+      Constants.PneumaticsSubsystem.SolenoidChannel
+  );
+  private Solenoid RightSolenoid = new Solenoid(
+      Constants.PneumaticsSubsystem.RightSolenoidID, 
+      PneumaticsModuleType.REVPH, 
+      Constants.PneumaticsSubsystem.SolenoidChannel
+  );
+  //Rev Pneumatic controller initialization
+  private PneumaticsControlModule PnController = new PneumaticsControlModule( 
+      Constants.PneumaticsSubsystem.PnControllerID
+  );
 
-    public static void CloseClaws() {
-        leftSolenoid.set(false);
-        RightSolenoid.set(false);
-    }
+  //methods to open and close the claws
+  public static void Openclaws() {
+      leftSolenoid.set(true);
+      RightSolenoid.set(true);
+  }
+
+  public static void CloseClaws() {
+      leftSolenoid.set(false);
+      RightSolenoid.set(false);
+  }
+
+  @Override
+  public void periodic() {
+  // This method will be called once per scheduler run
+  }
 }
+
+
+
